57073f0ae66c249b5841d4f94ae7616cf490f15c
kangkan <kangkankarmaker00@gmail.com>
2025-03-03 11:06:24 +0600

    new changes

--- app/src/main/java/com/mnr/imageslider/MainActivity.kt
+++ app/src/main/java/com/mnr/imageslider/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mnr.imageslider
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import com.mnr.imageslider.databinding.ActivityMainBinding
@@ -8,7 +9,7 @@ import com.mnr.imageslider.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val images: List<String> = listOf(
-        "https://cdn.pixabay.com/photo/2025/02/03/22/59/mountain-9380557_960_720.jpg",
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dhiwise.com%2Fpost%2Fstreamlining-image-selection-with-flutter-image-picker-plugin&psig=AOvVaw1hR-J0j2iO78qPDc4IVoRJ&ust=1741061924874000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDCosmH7YsDFQAAAAAdAAAAABAE",
         "https://cdn.pixabay.com/photo/2024/07/01/07/11/cats-8864617_1280.jpg",
         "https://cdn.pixabay.com/photo/2023/12/25/10/27/dog-8468288_960_720.jpg",
         "https://cdn.pixabay.com/photo/2024/01/08/21/31/frozen-8496388_960_720.jpg"
@@ -21,9 +22,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        binding.viewPagerId.adapter = ViewPagerAdapter(images)
-        TabLayoutMediator(binding.tabDots, binding.viewPagerId) { _, _ -> }.attach()
-        autoSlide(binding.viewPagerId, images.size)
+        binding.layoutImageSlider.viewPagerId.adapter = ViewPagerAdapter(images){
+            Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+        }
+        TabLayoutMediator(binding.layoutImageSlider.tabDots, binding.layoutImageSlider.viewPagerId) { _, _ -> }.attach()
+        autoSlide(viewPager2 = binding.layoutImageSlider.viewPagerId, listSize = images.size)
     }
 
 }
--- app/src/main/java/com/mnr/imageslider/ViewPagerAdapter.kt
+++ app/src/main/java/com/mnr/imageslider/ViewPagerAdapter.kt
@@ -8,13 +8,16 @@ import com.bumptech.glide.Glide
 import com.mnr.imageslider.databinding.ViewPagerItemBinding
 
 class ViewPagerAdapter(
-    private val list: List<String>
+    private val list: List<String>,
+    private val onItemClicked: (Int) -> Unit
 ) : RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val binding = ViewPagerItemBinding.bind(itemView)
         fun bind(url: String) {
             Glide.with(binding.root.context)
                 .load(url)
+                .error(R.drawable.baseline_error_24)
+                .placeholder(R.drawable.baseline_network_check_24)
                 .into(binding.imgId)
 
         }
@@ -29,5 +32,9 @@ class ViewPagerAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(list[position])
+
+        holder.itemView.setOnClickListener {
+            onItemClicked(position)
+        }
     }
 }
